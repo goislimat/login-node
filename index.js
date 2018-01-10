bfdd9867b63d5d49a8a015e190c6261f731b76a2
Thiago Gois <goislimat2@gmail.com>
2018-01-10 17:38:41 -0200

    setup of nodemon and route handlers for passport

--- index.js
+++ index.js
@@ -8,15 +8,30 @@ const app = express();
 passport.use(
   new GoogleStrategy(
     {
+      // Credentials to access Google OAuth
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback"
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      // Method that gets accessed when the user successfuly logs in
+      console.log("accessToken:", accessToken);
+      console.log("refreshToken:", refreshToken);
+      console.log("profile:", profile);
     }
   )
 );
+
+// Route called when the user tries to authenticate
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+// Route called when the user is redirected
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
