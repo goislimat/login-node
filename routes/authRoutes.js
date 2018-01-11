53316993c3b8e97a0e489eba0b4cdc2b69cd9e71
Thiago Gois <goislimat2@gmail.com>
2018-01-11 13:10:52 -0200

    eslint corrected

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,6 +12,11 @@ module.exports = app => {
 	// Route called when the user is redirected
 	app.get("/auth/google/callback", passport.authenticate("google"));
 
+	app.get("/api/logout", (req, res) => {
+		req.logout();
+		res.send(req.user);
+	});
+
 	// Route to get the current user
 	app.get("/api/current_user", (req, res) => {
 		res.send(req.user);
