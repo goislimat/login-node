e1ecb9d3d33439ca8016c733ea9f0539ef8880de
Thiago Gois <goislimat2@gmail.com>
2018-01-11 15:49:48 -0200

    login page route created

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-const App = () => <div>Hi There</div>;
+import LoginPage from "./pages/login";
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Route exact="/login" component={LoginPage} />
+      </div>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
