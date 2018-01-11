9980631275c9381c39bacd9f925a7bf1802bff57
Thiago Gois <goislimat2@gmail.com>
2018-01-11 13:04:27 -0200

    configured route to get the user and eslint

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,19 @@
 const passport = require("passport");
 
 module.exports = app => {
-  // Route called when the user tries to authenticate
-  app.get(
-    "/auth/google",
-    passport.authenticate("google", {
-      scope: ["profile", "email"]
-    })
-  );
+	// Route called when the user tries to authenticate
+	app.get(
+		"/auth/google",
+		passport.authenticate("google", {
+			scope: ["profile", "email"]
+		})
+	);
 
-  // Route called when the user is redirected
-  app.get("/auth/google/callback", passport.authenticate("google"));
+	// Route called when the user is redirected
+	app.get("/auth/google/callback", passport.authenticate("google"));
+
+	// Route to get the current user
+	app.get("/api/current_user", (req, res) => {
+		res.send(req.user);
+	});
 };
