2338332ea6c1f1ee8b828e230f546aa427aee92c
Thiago Gois <goislimat2@gmail.com>
2018-01-15 23:54:33 -0200

    added some auth tests

--- test/auth.js
+++ test/auth.js
@@ -10,113 +10,256 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe("Auth", () => {
-  beforeEach(done => {
-    User.remove({}, err => {
-      done();
+describe("Auth Tests", () => {
+  describe("unsuccessful attempts", () => {
+    beforeEach(done => {
+      User.remove({}, err => {
+        done();
+      });
+    });
+
+    describe("creating an user", () => {
+      it("should reject empty email", done => {
+        const credentials = {
+          email: "",
+          password: "12345678"
+        };
+
+        chai
+          .request(server)
+          .post("/api/signup")
+          .send(credentials)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.an("object");
+            res.body.user.should.be.false;
+            res.body.info.message.should.equal(
+              "E-mail/Password cant't be blank"
+            );
+            done();
+          });
+      });
+
+      it("should reject empty password", done => {
+        const credentials = {
+          email: "goislimat@gmail.com",
+          password: ""
+        };
+
+        chai
+          .request(server)
+          .post("/api/signup")
+          .send(credentials)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.an("object");
+            res.body.user.should.be.false;
+            res.body.info.message.should.equal(
+              "E-mail/Password cant't be blank"
+            );
+            done();
+          });
+      });
+
+      it("should reject invalid email", done => {
+        const credentials = {
+          email: "goislimat",
+          password: "12345678"
+        };
+
+        chai
+          .request(server)
+          .post("/api/signup")
+          .send(credentials)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.an("object");
+            res.body.user.should.be.false;
+            res.body.info.message.should.equal("This is not a valid e-mail");
+            done();
+          });
+      });
+
+      it("should reject password with less than 8 characters", done => {
+        const credentials = {
+          email: "goislimat@gmail.com",
+          password: "1234567"
+        };
+
+        chai
+          .request(server)
+          .post("/api/signup")
+          .send(credentials)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.an("object");
+            res.body.user.should.be.false;
+            res.body.info.message.should.equal(
+              "Password should have at least 8 characters"
+            );
+            done();
+          });
+      });
+    });
+
+    describe("login in an user", () => {
+      it("should reject login with an inexisting user", done => {
+        const credentials = {
+          email: "goislimat@gmail.com",
+          password: "12345678"
+        };
+
+        chai
+          .request(server)
+          .post("/api/login")
+          .send(credentials)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.an("object");
+            res.body.user.should.be.false;
+            res.body.info.message.should.equal(
+              "Invalid email/password combination"
+            );
+            done();
+          });
+      });
+
+      it("should reject empty email", done => {
+        const credentials = {
+          email: "",
+          password: "12345678"
+        };
+
+        chai
+          .request(server)
+          .post("/api/login")
+          .send(credentials)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.an("object");
+            res.body.user.should.be.false;
+            res.body.info.message.should.equal(
+              "E-mail/Password cant't be blank"
+            );
+            done();
+          });
+      });
+
+      it("should reject empty password", done => {
+        const credentials = {
+          email: "goislimat@gmail.com",
+          password: ""
+        };
+
+        chai
+          .request(server)
+          .post("/api/login")
+          .send(credentials)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.an("object");
+            res.body.user.should.be.false;
+            res.body.info.message.should.equal(
+              "E-mail/Password cant't be blank"
+            );
+            done();
+          });
+      });
+
+      it("should reject invalid email", done => {
+        const credentials = {
+          email: "goislimat",
+          password: "12345678"
+        };
+
+        chai
+          .request(server)
+          .post("/api/login")
+          .send(credentials)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.an("object");
+            res.body.user.should.be.false;
+            res.body.info.message.should.equal("This is not a valid e-mail");
+            done();
+          });
+      });
+
+      it("should reject password with less than 8 characters", done => {
+        const credentials = {
+          email: "goislimat@gmail.com",
+          password: "1234567"
+        };
+
+        chai
+          .request(server)
+          .post("/api/login")
+          .send(credentials)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.an("object");
+            res.body.user.should.be.false;
+            res.body.info.message.should.equal(
+              "Password should have at least 8 characters"
+            );
+            done();
+          });
+      });
     });
   });
 
-  describe("POST to login an inexisting user", () => {
-    it("should try to login", done => {
+  describe("successful attempts", () => {
+    describe("creating an user", () => {
+      beforeEach(done => {
+        User.remove({}, err => {
+          done();
+        });
+      });
+
+      it("should accept valid credentials", done => {
+        const credentials = {
+          email: "goislimat@gmail.com",
+          password: "12345678"
+        };
+
+        chai
+          .request(server)
+          .post("/api/signup")
+          .send(credentials)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.an("object");
+            res.body.user.should.include({ email: credentials.email });
+            done();
+          });
+      });
+    });
+
+    describe("loging in an user", () => {
       const credentials = {
         email: "goislimat@gmail.com",
         password: "12345678"
       };
 
-      chai
-        .request(server)
-        .post("/api/login")
-        .send(credentials)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.an("object");
-          res.body.user.should.be.false;
-          res.body.info.message.should.equal(
-            "Invalid email/password combination"
-          );
+      it("should login the created user", done => {
+        chai
+          .request(server)
+          .post("/api/login")
+          .send(credentials)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.an("object");
+            res.body.user.should.include({ email: credentials.email });
+            done();
+          });
+      });
+
+      it("should have exactly 1 user in database", done => {
+        User.find({}, (err, users) => {
+          users.length.should.equal(1);
           done();
         });
-    });
-  });
-
-  describe("Create an User", () => {
-    it("should reject empty email", done => {
-      const credentials = {
-        email: "",
-        password: "12345678"
-      };
-
-      chai
-        .request(server)
-        .post("/api/signup")
-        .send(credentials)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.an("object");
-          res.body.user.should.be.false;
-          res.body.info.message.should.equal("E-mail/Password cant't be blank");
-          done();
-        });
-    });
-
-    it("should reject empty password", done => {
-      const credentials = {
-        email: "goislimat@gmail.com",
-        password: ""
-      };
-
-      chai
-        .request(server)
-        .post("/api/signup")
-        .send(credentials)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.an("object");
-          res.body.user.should.be.false;
-          res.body.info.message.should.equal("E-mail/Password cant't be blank");
-          done();
-        });
-    });
-
-    it("should reject invalid email", done => {
-      const credentials = {
-        email: "goislimat",
-        password: "12345678"
-      };
-
-      chai
-        .request(server)
-        .post("/api/signup")
-        .send(credentials)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.an("object");
-          res.body.user.should.be.false;
-          res.body.info.message.should.equal("This is not a valid e-mail");
-          done();
-        });
-    });
-
-    it("should reject password with less than 8 characters", done => {
-      const credentials = {
-        email: "goislimat@gmail.com",
-        password: "1234567"
-      };
-
-      chai
-        .request(server)
-        .post("/api/signup")
-        .send(credentials)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.an("object");
-          res.body.user.should.be.false;
-          res.body.info.message.should.equal(
-            "Password should have at least 8 characters"
-          );
-          done();
-        });
+      });
     });
   });
 });
